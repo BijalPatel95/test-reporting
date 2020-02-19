665684022a8e820b06945e173fc3f9fb088044c9
BijalPatel95 <58430443+BijalPatel95@users.noreply.github.com>
2020-02-19 20:37:32 +0530

    Changes

--- Invoke/src/impersonate.js
+++ Invoke/src/impersonate.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 async function login(page, user,uname,pass) {
     await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: '/tmp/downloads'});    
-    await page.goto(process.env.edsAdmin);
     console.log('on'+process.env.edsAdmin)
+    await page.goto(process.env.edsAdmin,{waitUntil: 'load'});
     await page.type('input[name=username]', uname, {delay: 20});
     await page.type('input[name=password]', pass, {delay: 20});
     console.log('cred done')
