164bc64b140f9e91f371ccceafbe5f8287a1f6ea
BijalPatel95 <58430443+BijalPatel95@users.noreply.github.com>
2020-02-19 19:07:37 +0530

    Changes to trigger.js

--- ConnectDatabase/trigger.js
+++ ConnectDatabase/trigger.js
@@ -35,7 +35,7 @@ exports.handler = async (event, context, callback) => {
     for (let report of reportList){
         i = i+1;
         asyncFunctions.push(
-            startExecution(`Try3_${i}_${(new Date().toISOString().split('T')[0])}`, report)
+            startExecution(`Try4_${i}_${(new Date().toISOString().split('T')[0])}`, report)
         )
     }
 
