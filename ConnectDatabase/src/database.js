e2a03b9a03100a74baf84923e2bc8f2bfb0593df
BijalPatel95 <58430443+BijalPatel95@users.noreply.github.com>
2020-02-21 18:42:28 +0530

    Connecting through SSM

--- ConnectDatabase/src/database.js
+++ ConnectDatabase/src/database.js
@@ -1,12 +1,32 @@
+// const sql = require('mssql')
+// const dbConfig = require("./dbConfig")
+
+// class Database {
+//     static async runQuery(query) {
+//         try {
+//             const config = await dbConfig.getDBConfig();
+//             console.log(config);
+//             await sql.connect(config);
+//             console.log('connected')
+//             const result = await sql.query(query)
+//             sql.close()
+//             return result["recordset"]
+//         } catch (e) {
+//             //slack.notifyError(e);
+//             throw e
+//         }
+//     }
+// }
+// module.exports = Database
+
+
 const sql = require('mssql')
 const dbConfig = require("./dbConfig")
 
 class Database {
     static async runQuery(query) {
         try {
-            const config = await dbConfig.getDBConfig();
-            console.log(config);
-            await sql.connect(config);
+            await sql.connect(dbConfig.configuration)
             const result = await sql.query(query)
             sql.close()
             return result["recordset"]
