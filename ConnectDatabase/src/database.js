82319790a219f8126db748cb9c814721c868d10d
BijalPatel95 <58430443+BijalPatel95@users.noreply.github.com>
2020-02-21 17:25:03 +0530

    Connecting through SSM

--- ConnectDatabase/src/database.js
+++ ConnectDatabase/src/database.js
@@ -4,8 +4,14 @@ const dbConfig = require("./dbConfig")
 class Database {
     static async runQuery(query) {
         try {
-            const config = await dbConfig.getDBConfig();
-            console.log(config);
+            // const config = await dbConfig.getDBConfig();
+            // console.log(config);
+
+            const config = { DATABASE_NAME: 'Quant',
+            DATABASE_SERVER: '40.121.1.186',
+            DATABASE_PASSWORD: 'Init@123',
+            DATABASE_USER: 'test' };
+            
             await sql.connect(config);
             const result = await sql.query(query)
             sql.close()
